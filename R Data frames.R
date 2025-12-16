4a810221d7883f8554cc451cc2222b06e9766575
Felipe <felipe.iraola@gmail.com>
2025-12-16 20:18:53 -0300

    Se cargan archivos de trabajo

--- R Data frames.R
+++ R Data frames.R
@@ -1,3 +1,36 @@
-data()
-Orange
-str(Orange)
+df<-read.csv("StudentPerformance.csv") #Cargar archivo
+
+write.csv(df,file="StudentPerformance.csv")
+
+df
+
+nrow(df)
+
+colnames(df)
+
+str(df)
+
+summary(df)
+
+df
+
+df[[5,2]]
+
+df[[5,"Hours.Studied"]]<-9
+
+#Referenciando rows
+
+df[,2]
+cars.df<-mtcars
+
+head(cars.df)
+
+mtcars$mpg #Seleccionar columnas mas comun
+
+df$Hours.Studied
+
+mtcars[,"mpg"]
+
+
+data.mtcars<-mtcars["mpg"]
+data.mtcars
