d9ce00596846ca8062c67ce55047ef74c7443082
Felipe <felipe.iraola@gmail.com>
2025-12-16 20:57:09 -0300

    Clase data frames

---
+++ R Data frames part2.R
@@ -0,0 +1,7 @@
+df2<-data.frame(col.name.1=2000,col.name.2="New")
+
+df2e
+
+dfnew<-rbind(df,df2)
+sds
+df2$newcol<-1*df$col.name1
--- R Data frames.R
+++ R Data frames.R
@@ -1,36 +1,79 @@
-df<-read.csv("StudentPerformance.csv") #Cargar archivo
+#Overview data frames operaciones
 
-write.csv(df,file="StudentPerformance.csv")
+#Creating DF
+
+empty <- data.frame()
+c1 <- 1:10
+c1
+letters
+c2 <- letters[1:10]
+c1
+c2
+
+df <- data.frame(col.name.1=c1, col.name.2=c2)
+df
+
+# 2 Importing and export data
+
+d2 <- read.csv("StudentPerformance.csv")
+
+writeDF <- write.csv(df,file="saved_df.csv") #Crea un archivo CSV
+
+df2<-read.csv("saved_df.csv")
+df
+df2 #Extra columna X index data frame
+
+# 3 Gettinf informacion about DF
 
 df
 
 nrow(df)
-
+ncol(df)
 colnames(df)
-
+rownames(df)
 str(df)
 
 summary(df)
 
-df
+
+#4 Referenciar celdas
 
 df[[5,2]]
 
-df[[5,"Hours.Studied"]]<-9
-
-#Referenciando rows
-
-df[,2]
-cars.df<-mtcars
-
-head(cars.df)
-
-mtcars$mpg #Seleccionar columnas mas comun
-
-df$Hours.Studied
-
-mtcars[,"mpg"]
 
 
-data.mtcars<-mtcars["mpg"]
-data.mtcars
+df[[5,"col.name.2"]]
+
+
+#5 Referenciando filas
+df[[2,"col.name.1"]] <- 9999
+df
+
+df[[2,"col.name.2"]] <- "b"
+df
+
+
+df[[5,"col.name.2"]] <- "e"
+df
+
+
+df[1,]
+
+#6 Referenciando columnas
+mtcars
+
+head(mtcars)
+
+
+mtcars$mpg #forma mas comun
+mtcars[,3]
+mtcars[["mpg"]]
+
+mtcars["hp"]
+
+mtcars[1]
+
+
+#Llamar varias columnas
+
+head(mtcars[c("mpg","cyl")])
