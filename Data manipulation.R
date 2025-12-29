de2337c8c68f7c8223fbc1a93cef8dc4ac0f86cd
Felipe <felipe.iraola@gmail.com>
2025-12-28 21:31:28 -0300

    Data manipulation

--- Data manipulation.R
+++ Data manipulation.R
@@ -14,3 +14,18 @@ slice(flights,1:3)
 
   
 head(arrange(flights, year,month,day,arr_time))
+
+head(select(flights,carrier, arr_time))
+
+head(rename(flights,airline_carrier= carrier))
+
+distinct(flights,carrier)
+
+head(mutate(flights,nueva_columna=arr_delay-dep_delay))
+
+head(transmute(flights,nueva_columna=arr_delay*dep_delay))
+
+summarise(flights,total_time=sum(air_time,na.rm=T))
+
+sample_frac(flights,0.1)
+
