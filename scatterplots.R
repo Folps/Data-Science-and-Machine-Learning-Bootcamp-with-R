426fa18fed6d6cc38853299365bceb2e91fc9cbb
Felipe Iraola <felipe.iraola@gmail.com>
2026-01-02 19:21:18 -0300

    Scatterplots

--- scatterplots.R
+++ scatterplots.R
@@ -4,4 +4,13 @@ library(ggplot2)
 df <- mtcars
 
 # DATA % AESTHETICKS
-ggplot(data=df)
+pl <- ggplot(df,aes(x=wt, y=mpg))
+
+#geometry
+
+#print(pl+ geom_point(apha=0.5, size=0.5))
+
+pl2 <- pl + geom_point(aes(color=hp), size =2)
+
+print(pl2)
+                      
